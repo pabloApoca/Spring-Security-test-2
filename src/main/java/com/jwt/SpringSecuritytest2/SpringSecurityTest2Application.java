692a5dd9f04d676ad5d6a0c1df4199ba88564db2
pabloApoca <pablo_mtv08@hotmail.com>
2023-07-10 20:53:06 -0300

    Base proyecto Spring Security.

---
+++ src/main/java/com/jwt/SpringSecuritytest2/SpringSecurityTest2Application.java
@@ -0,0 +1,13 @@
+package com.jwt.SpringSecuritytest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityTest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityTest2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jwt/SpringSecuritytest2/SpringSecurityTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwt.SpringSecuritytest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
